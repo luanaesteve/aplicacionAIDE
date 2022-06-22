d8889eea596544cbb6c78ef439d624ea63bba4e3
luana <luanaesteve@hotmail.com>
2022-06-22 18:21:30 -0300

    jaja

--- miaplicacion/src/UI/Components/LoginForm.js
+++ miaplicacion/src/UI/Components/LoginForm.js
@@ -2,5 +2,13 @@ import React from "react";
 import {TextInput, Button} from "react-native"
 
 export default function LoginForm(){
-    
+    return(
+        <view>
+            <text>hola</text>
+            <text>adios</text>
+            <text>ajja</text>
+            <TextInput placeholder="Email"></TextInput>
+            <button>Enviar</button>
+        </view>
+    )
 }
