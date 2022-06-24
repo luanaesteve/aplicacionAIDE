b5c3ec71bbe3f0e745ec99aca3a12e0eba63ff04
luana <luanaesteve@hotmail.com>
2022-06-24 03:36:39 -0300

    jajjaa

---
+++ miaplicacion/src/App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import LoginForm from "./UI/Components/LoginForm";
+
+export default function App(){
+    return(
+        <view style={StyleSheet.container}>
+            
+        </view>
+    )
+}
